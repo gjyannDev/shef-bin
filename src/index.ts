d2f1ffada24f34a45c7e5a3a944d77d47813a8c6
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-23 14:27:18 +0800

    chore: able to run typescript with tailwindcss

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import dotenv from "dotenv";
 import express from "express";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.urlencoded({ extended: true }));
+
+// const __filename = fileURLToPath(import.meta.url)
 
 app.get("/", (req, res) => {
   res.send("HELLO WORLD");
