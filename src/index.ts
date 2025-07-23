e48ebfb4594df8381b4370033c24aebb6166898f
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-23 15:18:35 +0800

    feat: finish setting up the typescript with tailwindcss in express js environment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import dotenv from "dotenv";
 import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 
@@ -8,8 +10,16 @@ const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: true }));
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "styles")));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("HELLO World");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
