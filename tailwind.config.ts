08d6ef163424eeabfdfdff01d422a8a166d24919
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-23 23:01:18 +0800

    chore: set up some few things for the coding environment and added some files needed

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+export default {
+  content: ["./views/**/*.ejs"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
