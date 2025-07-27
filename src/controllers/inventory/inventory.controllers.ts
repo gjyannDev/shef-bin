ec064cbd5960aceae9cdda2b7b1dcc48e1954846
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-27 15:15:10 +0800

    chore: able to fetch all items and can fetch it on the EJS file

--- src/controllers/inventory/inventory.controllers.ts
+++ src/controllers/inventory/inventory.controllers.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
+import { getAllItems } from "../../services/items.services.js";
 
 export async function getInventoryPage(req: Request, res: Response) {
   try {
-    return res.render("pages/inventory");
+    const items = await getAllItems();
+    
+    return res.render("pages/inventory", { items });
   } catch (error) {
     console.error(error);
     res.status(500);
