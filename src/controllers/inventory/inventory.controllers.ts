b996a4b94cb225531ba7ffc3f5affaac73e4c9ea
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-27 18:33:10 +0800

    chore: able to fetch all items and display it on inventory page

--- src/controllers/inventory/inventory.controllers.ts
+++ src/controllers/inventory/inventory.controllers.ts
@@ -1,11 +1,15 @@
 import { Request, Response } from "express";
 import { getAllItems } from "../../services/items.services.js";
+import { formatDate } from "../../utils/dateFormatter.js";
 
 export async function getInventoryPage(req: Request, res: Response) {
   try {
     const items = await getAllItems();
-    
-    return res.render("pages/inventory", { items });
+
+    return res.render("pages/inventory", { items: items?.map((item) => ({
+      ...item.toObject(),
+      formattedTime: formatDate(item.expirationDate)
+    }))});
   } catch (error) {
     console.error(error);
     res.status(500);
