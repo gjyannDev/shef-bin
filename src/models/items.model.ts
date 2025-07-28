15cc99e6c334f056fffa9edd93455596590c85eb
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-07-28 23:19:15 +0800

    chore: removed the timestamps and add formattedExpirationDate

--- src/models/items.model.ts
+++ src/models/items.model.ts
@@ -1,41 +1,42 @@
 import mongoose from "mongoose";
 
-const item_schema = new mongoose.Schema(
-  {
-    itemName: {
-      type: String,
-      required: [true, "Item name is required"],
-    },
-    category: {
-      type: String,
-      required: true,
-    },
-    quantity: {
-      type: Number,
-      required: [true, "Quantity is required"],
-      min: [0, "Quantity cannot be negative"],
-    },
-    unit: {
-      type: String,
-      required: [true, "Unit is required"],
-      enum: ["pcs", "pc", "kg", "g", "l", "ml", "packs", "bottles", "bunch"],
-    },
-    expirationDate: {
-      type: Date,
-      required: [true, "Expiration date is required"],
-    },
-    notes: {
-      type: String,
-      default: "",
-    },
-    status: {
-      type: String,
-      required: true,
-      enum: ["in stock", "low stock", "expired", "out of stock"],
-      default: "in stock",
-    },
+const item_schema = new mongoose.Schema({
+  itemName: {
+    type: String,
+    required: [true, "Item name is required"],
   },
-  { timestamps: true }
-);
+  category: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Quantity is required"],
+    min: [0, "Quantity cannot be negative"],
+  },
+  unit: {
+    type: String,
+    required: [true, "Unit is required"],
+    enum: ["pcs", "pc", "kg", "g", "l", "ml", "packs", "bottles", "bunch"],
+  },
+  expirationDate: {
+    type: Date,
+    required: [true, "Expiration date is required"],
+  },
+  notes: {
+    type: String,
+    default: "",
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["in stock", "low stock", "expired", "out of stock"],
+    default: "in stock",
+  },
+  formattedExpirationDate: {
+    type: String,
+    required: true,
+  },
+});
 
 export const Item = mongoose.model("Items", item_schema);
